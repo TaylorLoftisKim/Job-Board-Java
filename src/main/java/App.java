12dc48f4a10f7bc437c15de7d41fbb4370a580d0
Andrew Malkin and Taylor Loftis-Kim <andrew@>
2017-03-22 11:03:20 -0700

    Add Front end logic for app, Create fully functional job board app.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,67 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("cities/:id/job-openings/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city-job-openings-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/job-openings", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("jobOpenings", JobOpening.all());
+      model.put("template", "templates/job-openings.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/job-openings", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.queryParams("cityId")));
+      String jobTitle = request.queryParams("jobTitle");
+      JobOpening newJobOpening = new JobOpening(jobTitle);
+      city.addJobOpening(newJobOpening);
+      model.put("city", city);
+      model.put("template", "templates/city-job-opening-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/job-openings/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      JobOpening jobOpening = JobOpening.find(Integer.parseInt(request.params(":id")));
+      model.put("jobOpening", jobOpening);
+      model.put("template", "templates/job-opening.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/city-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/cities", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      City newCity = new City(name);
+      model.put("template", "templates/city-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("cities", City.all());
+      model.put("template", "templates/cities.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
